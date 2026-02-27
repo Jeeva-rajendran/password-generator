a4427b96dba2a631374c59e0b94e6248603fd211
Jeeva-rajendran <jeevarajendran2004@gmail.com>
2026-02-27 10:42:30 +0530

    Converted to Function

--- password.py
+++ password.py
@@ -1,13 +1,18 @@
 import secrets
 import string
 
-# Define the character set: letters, digits, and punctuation
-characters = string.ascii_letters + string.digits + string.punctuation
+def password_generator():
+    # Define the character set: letters, digits, and punctuation
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Prompt the user for the desired password length 
+    length = int(input("Enter the length of the password: "))
+    
+    # Generate a secure password
+    return ''.join(secrets.choice(characters) for _ in range(length))
 
-# Prompt the user for the desired password length
-length = int(input("Enter the length of the password: "))
+password = password_generator()
 
-# Generate a secure password
-password = ''.join(secrets.choice(characters) for _ in range(length))
-
-print("Generated password:", password)
+print(password)
+    
+    
